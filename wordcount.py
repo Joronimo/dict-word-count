d46ba809c288e7025649254bb43e3448f16012a0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 12:34:29 -0700

    Removed punctuation

--- wordcount.py
+++ wordcount.py
@@ -2,15 +2,19 @@
 
 
 def word_count(file):
-    file = open(file)
+    file = open(file) # blob of file
 
     dict_word_count = {}
-
+    exclude = ",.?!:;"
     for line in file:
-        lines = line.lower()
-        row = lines.split(',')
+        lines = line.lower() # removes case sensitivity ( => string) 
+        row = lines.split() # list of words in a line ( => list)
+
 
         for word in row:
+            for char in word:
+                if char in exclude:
+                    word = word[0:-1]
             dict_word_count[word] = dict_word_count.get(word, 0) + 1
 
     return dict_word_count
