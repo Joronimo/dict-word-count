78dcde8eaf3dba31997695947895a0ff4e67cf8a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-24 12:03:13 -0700

    First commit

---
+++ wordcount.py
@@ -0,0 +1,16 @@
+#open file
+
+
+def word_count(file):
+    file = open(file)
+    
+    dict_word_count = {}
+
+    for line in file:
+        row = line.split()
+
+        for word in row:
+            dict_word_count[word] = dict_word_count.get(word, 0) + 1
+
+    return dict_word_count
+print(word_count('test.txt'))
